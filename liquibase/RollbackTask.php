72cac35b6c3a2280f8fe0cd7e936f18b2208a88f
Stephan Hochdörfer <S.Hochdoerfer@bitExpert.de>
2011-05-23 16:57:53 +0200

    added rollback and tag tasks

---
+++ liquibase/RollbackTask.php
@@ -0,0 +1,70 @@
+<?php
+require_once dirname(__FILE__).'/AbstractTask.php';
+/*
+* Copyright (c) 2007-2011 bitExpert AG
+*
+* Licensed under the Apache License, Version 2.0 (the "License"); you may not
+* use this file except in compliance with the License. You may obtain a copy of
+* the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+* License for the specific language governing permissions and limitations under
+* the License.
+*/
+
+
+/**
+* Rollbacks the database changes.
+*
+* @author Stephan Hochdoerfer <S.Hochdoerfer@bitExpert.de>
+*/
+
+
+class RollbackTask extends AbstractTask
+{
+	protected $rollbackTag;
+
+
+	/**
+	 * Sets the name of the tag to roll back to.
+	 *
+	 * @param string the output directory
+	 */
+	public function setRollbackTag($rollbackTag)
+	{
+		$this->rollbackTag = $rollbackTag;
+	}
+
+
+	/**
+	 * @see AbstractTask::checkParams()
+	 */
+	protected function checkParams()
+	{
+		parent::checkParams();
+
+		if(null === $this->rollbackTag)
+		{
+			throw new BuildException(
+				sprintf(
+					'Please specify the tag to rollback to!',
+					$this->rollbackTag
+				)
+			);
+		}
+	}
+
+
+	/**
+	 * @see Task::main
+	 */
+	public function main()
+	{
+		$this->checkParams();
+		$this->execute('rollback', escapeshellarg($this->rollbackTag));
+	}
+}
